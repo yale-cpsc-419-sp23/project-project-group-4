ad984fe50a45b32f0f6f96c5742dff28a70cc8fb
chris544460 <christianedu.122@gmail.com>
2023-02-26 15:42:12 -0500

    making post form...

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, LostObjects
 from . import db
 import json
 
@@ -35,3 +35,30 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField
+from wtforms.validators import DataRequired
+
+class PostLossForm(FlaskForm):
+    loster = StringField('Lost by', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    place = SelectField('Place', coerce=int, validators=[DataRequired()])
+    classifier = SelectField('Classifier', coerce=int, validators=[DataRequired()])
+
+
+@views.route('/post-loss', methods=['GET', 'POST'])
+def post_loss():
+    form = PostLossForm()
+    if form.validate_on_submit():
+        lost_object = LostObjects(
+            loster=form.loster.data,
+            description=form.description.data,
+            place_id=form.place.data,
+            classifier_id=form.classifier.data
+        )
+        db.session.add(lost_object)
+        db.session.commit()
+        flash('Lost item posted successfully!', 'success')
+        # return redirect(url_for('views.home')) 
+    return render_template('post_loss.html', form=form)
