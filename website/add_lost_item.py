08a0356d41137cc3827fe11efbfe56f12cac78ea
Brian <brianchoi600@gmail.com>
2023-02-26 15:30:11 -0500

    updated get,post,delte

--- website/add_lost_item.py
+++ website/add_lost_item.py
@@ -7,23 +7,21 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 @app.route('/lost_objects', methods=['GET'])
-def get_lost():
-    lost_objects = LostObjects.query.all()
-    return lost_objects
+def get_lost(id):
+    lost = LostObjects.query.get_or_404(id)
+    return lost
 
 @app.route('/lost_objects', methods=['POST'])
 def create_lost(loster, description, lost_date, place_id, classifier_id):
-    new_lost_object = LostObjects(loster=loster, description=description, lost_date=lost_date, place_id=place_id, classifier_id=classifier_id)
+    new_lost = LostObjects(loster=loster, description=description, lost_date=lost_date, place_id=place_id, classifier_id=classifier_id)
 
-    db.session.add(new_lost_object)
+    db.session.add(new_lost)
     db.session.commit()
 
-    return new_lost_object
+    return new_lost
 
 @app.route('/lost_objects/<int:id>', methods=['DELETE'])
 def delete_lost(id):
     lost_object = LostObjects.query.get_or_404(id)
     db.session.delete(lost_object)
     db.session.commit()
-
-    return {'message': f'Lost object with ID {id} was successfully deleted.'}
