0f5fa65a75380a29c33fbd722a792da133b3ae93
Brian <brianchoi600@gmail.com>
2023-02-26 15:24:45 -0500

    get and post for lost objects

---
+++ website/add_lost_item.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from models import LostObjects
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+@app.route('/lost_objects', methods=['GET'])
+def get_lost_objects():
+    lost_objects = LostObjects.query.all()
+    return lost_objects
+
+@app.route('/lost_objects', methods=['POST'])
+def create_lost_object(loster, description, lost_date, place_id, classifier_id):
+    new_lost_object = LostObjects(loster=loster, description=description, lost_date=lost_date, place_id=place_id, classifier_id=classifier_id)
+
+    db.session.add(new_lost_object)
+    db.session.commit()
+
+    return new_lost_object
+
+@app.route('/lost_objects/<int:id>', methods=['DELETE'])
+def delete_lost_object(id):
+    lost_object = LostObjects.query.get_or_404(id)
+    db.session.delete(lost_object)
+    db.session.commit()
+
+    return {'message': f'Lost object with ID {id} was successfully deleted.'}
