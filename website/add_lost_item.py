159a5c92a26615c27f19e47ab85685858202d9b4
Brian <brianchoi600@gmail.com>
2023-02-26 15:27:33 -0500

    changed function names

--- website/add_lost_item.py
+++ website/add_lost_item.py
@@ -7,12 +7,12 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 @app.route('/lost_objects', methods=['GET'])
-def get_lost_objects():
+def get_lost():
     lost_objects = LostObjects.query.all()
     return lost_objects
 
 @app.route('/lost_objects', methods=['POST'])
-def create_lost_object(loster, description, lost_date, place_id, classifier_id):
+def create_lost(loster, description, lost_date, place_id, classifier_id):
     new_lost_object = LostObjects(loster=loster, description=description, lost_date=lost_date, place_id=place_id, classifier_id=classifier_id)
 
     db.session.add(new_lost_object)
@@ -21,7 +21,7 @@ def create_lost_object(loster, description, lost_date, place_id, classifier_id):
     return new_lost_object
 
 @app.route('/lost_objects/<int:id>', methods=['DELETE'])
-def delete_lost_object(id):
+def delete_lost(id):
     lost_object = LostObjects.query.get_or_404(id)
     db.session.delete(lost_object)
     db.session.commit()
